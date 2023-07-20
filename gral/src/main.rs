0887d50d7ad0ca9fe3a8ef0135926d2a8f5e2a7c
Max Neunhoeffer <max@arangodb.com>
2023-07-20 23:21:48 +0200

    Log level to trace.

--- gral/src/main.rs
+++ gral/src/main.rs
@@ -28,7 +28,7 @@ pub const VERSION: u32 = 0x00100;
 async fn main() {
     env_logger::Builder::new()
         .format_timestamp(Some(env_logger::fmt::TimestampPrecision::Micros))
-        .filter_level(LevelFilter::Info)
+        .filter_level(LevelFilter::Trace)
         .parse_env("RUST_LOG")
         .init();
     info!("Hello, this is gral!");
