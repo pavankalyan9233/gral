63b1d8de03f1671c6194745b4019fcd4d5ce3348
hkernbach <heiko@arangodb.com>
2024-04-02 13:18:38 +0200

    added auth service integration tests

---
+++ tests/authentication_test_auth_service.rs
@@ -0,0 +1,40 @@
+extern crate utilities;
+
+#[test]
+fn setup() {
+    // setup code here
+    std::thread::spawn(|| {
+        // Start service, but now use the auth-service docker environment for authentication.
+        std::env::set_var("INTEGRATION_SERVICE_ADDRESS", "localhost:9092");
+        gral::server::run();
+    });
+}
+
+#[test]
+fn test_authentication_no_auth_given() {
+    let response = utilities::http_helper::get("http://localhost:9999/v1/graphs", None);
+    assert_eq!(response["errorCode"].as_u64().unwrap(), 401);
+}
+
+#[test]
+fn test_authentication_with_invalid_bearer_token() {
+    let invalid_token = "invalid_token";
+    let headers = utilities::http_helper::build_bearer_auth_header(&invalid_token);
+    let response = utilities::http_helper::get("http://localhost:9999/v1/graphs", Some(headers));
+
+    assert_eq!(response["errorCode"].as_u64().unwrap(), 401);
+}
+
+#[test]
+fn test_authentication_with_bearer_token() {
+    let token = utilities::arangodb_helper::generate_superuser_bearer();
+    let headers = utilities::http_helper::build_bearer_auth_header(&token);
+    let response = utilities::http_helper::get("http://localhost:9999/v1/graphs", Some(headers));
+
+    assert_eq!(response.to_string(), "[]");
+}
+
+#[test]
+fn teardown() {
+    // teardown code here
+}
