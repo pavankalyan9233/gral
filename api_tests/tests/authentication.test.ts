2b80d249e5e3b4d8e638fa6f166c0c0331a3bc38
hkernbach <heiko@arangodb.com>
2024-04-04 13:42:49 +0200

    remove rust integration tests

--- api_tests/tests/authentication.test.ts
+++ api_tests/tests/authentication.test.ts
@@ -6,19 +6,18 @@ import axios from 'axios';
 
 const gral_valid_auth_endpoints = [config.gral_instances.arangodb_auth, config.gral_instances.service_auth];
 const gral_invalid_auth_endpoints = [config.gral_instances.service_auth_unreachable];
+describe('Authentication tests', () => {
+  describe('With valid JWT token', () => {
+    let jwt: String;
 
-describe('Authentication tests with ArangoDB auth configured', () => {
-  let jwt: String;
+    beforeAll(async () => {
+      jwt = await arangodb.getArangoJWT();
+    }, config.test_configuration.timeout);
 
-  beforeAll(async () => {
-    jwt = await arangodb.getArangoJWT();
-  }, config.test_configuration.timeout);
+    test('JWT token should be generated via call to ArangoDB itself', () => {
+      expect(jwt).not.toBe('');
+    });
 
-  test('JWT token should be generated via call to ArangoDB itself', () => {
-    expect(jwt).not.toBe('');
-  });
-
-  describe('Requests with valid JWT token', () => {
     test('GET /v1/graphs ', () => {
       for (let endpoint of gral_valid_auth_endpoints) {
         let url = gral.buildUrl(endpoint, '/v1/graphs');
@@ -28,17 +27,18 @@ describe('Authentication tests with ArangoDB auth configured', () => {
         });
       }
     });
+  });
 
-    describe('Requests with an invalid JWT token', () => {
-      test('GET /v1/graphs ', () => {
-        for (let endpoint of [...gral_valid_auth_endpoints, ...gral_invalid_auth_endpoints]) {
-          let url = gral.buildUrl(endpoint, '/v1/graphs');
-          axios.get(url, gral.buildHeaders('invalid')).catch((error) => {
-            expect(error.response.status).toBe(401);
-          });
-        }
-      });
+  describe('With an invalid JWT token', () => {
+    let jwt: String = 'invalid';
+
+    test('GET /v1/graphs ', () => {
+      for (let endpoint of [...gral_valid_auth_endpoints, ...gral_invalid_auth_endpoints]) {
+        let url = gral.buildUrl(endpoint, '/v1/graphs');
+        axios.get(url, gral.buildHeaders(jwt)).catch((error) => {
+          expect(error.response.status).toBe(401);
+        });
+      }
     });
-
   });
 });
