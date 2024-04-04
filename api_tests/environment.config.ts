14f946d1e67fcf9e98bc3c41f4fab73b8f240ce3
hkernbach <heiko@arangodb.com>
2024-04-04 23:08:31 +0200

    IP Test

--- api_tests/environment.config.ts
+++ api_tests/environment.config.ts
@@ -1,8 +1,8 @@
 export const config = {
   gral_instances: {
-    arangodb_auth: "http://localhost:9999",
-    service_auth: "http://localhost:1337",
-    service_auth_unreachable: "http://localhost:1336",
+    arangodb_auth: "http://127.0.0.1:9999",
+    service_auth: "http://127.0.0.1:1337",
+    service_auth_unreachable: "http://127.0.0.1:1336",
   },
   arangodb: {
     endpoint: "http://localhost:8529",
@@ -13,3 +13,6 @@ export const config = {
     timeout: 10000,
   }
 };
+
+// 1.) Check how to communicate in CI via different ports from base image to the service
+// 2.) Check how to set env specific config variable locally and in CI
