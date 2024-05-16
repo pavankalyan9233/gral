bd172e53a55e5d51a057482753dee8d82d327003
Julia Volmer <julia.volmer@arangodb.com>
2024-05-16 13:40:32 +0200

    Implement algorithm

--- src/algorithms/linerank/algorithm.rs
+++ src/algorithms/linerank/algorithm.rs
@@ -1,15 +1,229 @@
+//! Linerank implementation as described in "Centralities in Large Networks: Algorithms and Observations"
+//! by Kang et. al. (https://www.cs.cmu.edu/~ukang/papers/CentralitySDM2011.pdf)
+
+use crate::algorithms::linerank::graph_multiplications::EdgeVector;
 use crate::graph_store::graph::Graph;
+use std::ops::Add;
 
 pub fn line_rank(
-    g: &Graph,
-    supersteps: u32,
+    graph: &Graph,
+    max_supersteps: u32,
     damping_factor: f64,
 ) -> Result<(Vec<f64>, u32), String> {
-    Ok((vec![], 0))
+    let mut edge_rank = EdgeVector(vec![
+        1. / graph.number_of_edges() as f64;
+        graph.number_of_edges() as usize
+    ]);
+
+    let normalization = edge_normalization(graph);
+    let mut superstep = 0;
+    for step in 0..max_supersteps {
+        superstep = step;
+        let max_diff = next_superstep(&mut edge_rank, graph, &normalization, damping_factor);
+        if max_diff < 0.0000001 {
+            break;
+        }
+    }
+
+    let vertex_rank = edge_rank
+        .clone()
+        .apply_transposed_source_matrix(graph)
+        .add(edge_rank.apply_transposed_target_matrix(graph));
+
+    Ok((vertex_rank.0, superstep))
+}
+
+fn next_superstep(
+    old_rank: &mut EdgeVector,
+    graph: &Graph,
+    normalization: &EdgeVector,
+    damping: f64,
+) -> f64 {
+    let sum_over_incoming_ranks = old_rank
+        .clone()
+        .apply_transposed_source_matrix(graph)
+        .apply_target_matrix(graph)
+        .normalize_with(normalization);
+
+    let mut max_diff = 0.;
+    sum_over_incoming_ranks
+        .0
+        .into_iter()
+        .zip(old_rank.0.iter_mut())
+        .map(|(sum, old)| {
+            (
+                damping * sum + (1. - damping) * 1. / graph.number_of_edges() as f64,
+                old,
+            )
+        })
+        .for_each(|(new, old)| {
+            let diff = (new - *old).abs();
+            max_diff = if diff > max_diff { diff } else { max_diff };
+            *old = new;
+        });
+
+    max_diff
+}
+
+fn edge_normalization(graph: &Graph) -> EdgeVector {
+    EdgeVector(vec![1.; graph.number_of_edges() as usize])
+        .apply_transposed_source_matrix(graph)
+        .apply_target_matrix(graph)
+        .invert_elementwise()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use approx::assert_ulps_eq;
+
+    mod edge_normalization {
+        use super::*;
+
+        #[test]
+        fn is_empty_for_empty_graph() {
+            let graph = Graph::create(vec![], vec![]);
+            assert_eq!(edge_normalization(&graph).0.len(), 0);
+        }
+
+        #[test]
+        fn is_empty_for_unconnected_graph() {
+            let graph = Graph::create(vec!["V/A".to_string(), "V/B".to_string()], vec![]);
+            assert_eq!(edge_normalization(&graph).0.len(), 0);
+        }
+
+        #[test]
+        fn give_inverse_count_of_outgoing_edges_in_line_graph() {
+            let graph = Graph::create(
+                vec![
+                    "V/A".to_string(),
+                    "V/B".to_string(),
+                    "V/C".to_string(),
+                    "V/D".to_string(),
+                ],
+                vec![
+                    ("V/A".to_string(), "V/A".to_string()),
+                    ("V/A".to_string(), "V/B".to_string()),
+                    ("V/A".to_string(), "V/C".to_string()),
+                    ("V/C".to_string(), "V/A".to_string()),
+                ],
+            );
+
+            let normalization = edge_normalization(&graph);
+
+            assert_eq!(normalization.0.len() as u64, graph.number_of_edges());
+            assert_ulps_eq!(normalization.0[0], 1. / 3.);
+            assert_ulps_eq!(normalization.0[1], 0.);
+            assert_ulps_eq!(normalization.0[2], 1.);
+            assert_ulps_eq!(normalization.0[3], 1. / 3.);
+        }
+    }
+
+    mod aggregates_edge_importance_for_each_vertex {
+        use super::*;
+
+        #[test]
+        fn fork() {
+            let graph = Graph::create(
+                vec![
+                    "V/A".to_string(),
+                    "V/B".to_string(),
+                    "V/C".to_string(),
+                    "V/D".to_string(),
+                ],
+                vec![
+                    ("V/A".to_string(), "V/B".to_string()),
+                    ("V/B".to_string(), "V/C".to_string()),
+                    ("V/B".to_string(), "V/D".to_string()),
+                ],
+            );
+            let mut edge_rank = EdgeVector(vec![1. / 3.; graph.number_of_edges() as usize]);
+            let normalization = edge_normalization(&graph);
+
+            next_superstep(&mut edge_rank, &graph, &normalization, 0.85);
+            assert_ulps_eq!(edge_rank.0[0], 1. / 3.);
+            assert_ulps_eq!(edge_rank.0[1], 0.05);
+            assert_ulps_eq!(edge_rank.0[2], 0.05);
+            next_superstep(&mut edge_rank, &graph, &normalization, 0.85);
+            assert_ulps_eq!(edge_rank.0[0], 0.0925);
+            assert_ulps_eq!(edge_rank.0[1], 0.05);
+            assert_ulps_eq!(edge_rank.0[2], 0.05);
+
+            let (ranks, superstep) = dbg!(line_rank(&graph, 10, 0.85).unwrap());
+
+            assert_eq!(ranks.len(), 4);
+            assert_ulps_eq!(ranks[0], 0.0925);
+            assert_ulps_eq!(ranks[1], 0.1925);
+            assert_ulps_eq!(ranks[2], 0.05);
+            assert_ulps_eq!(ranks[3], 0.05);
+            assert_eq!(superstep, 2);
+        }
+
+        #[test]
+        fn one_line() {
+            let graph = Graph::create(
+                vec![
+                    "V/A".to_string(),
+                    "V/B".to_string(),
+                    "V/C".to_string(),
+                    "V/D".to_string(),
+                ],
+                vec![
+                    ("V/A".to_string(), "V/B".to_string()),
+                    ("V/B".to_string(), "V/C".to_string()),
+                    ("V/C".to_string(), "V/D".to_string()),
+                ],
+            );
+            let mut edge_rank = EdgeVector(vec![1. / 3.; graph.number_of_edges() as usize]);
+            let normalization = edge_normalization(&graph);
+
+            next_superstep(&mut edge_rank, &graph, &normalization, 0.85);
+            assert_ulps_eq!(edge_rank.0[0], 1. / 3.);
+            assert_ulps_eq!(edge_rank.0[1], 1. / 3.);
+            assert_ulps_eq!(edge_rank.0[2], 0.05);
+            next_superstep(&mut edge_rank, &graph, &normalization, 0.85);
+            assert_ulps_eq!(edge_rank.0[0], 1. / 3.);
+            assert_ulps_eq!(edge_rank.0[1], 0.0925);
+            assert_ulps_eq!(edge_rank.0[2], 0.05);
+            next_superstep(&mut edge_rank, &graph, &normalization, 0.85);
+            assert_ulps_eq!(edge_rank.0[0], 0.128625);
+            assert_ulps_eq!(edge_rank.0[1], 0.0925);
+            assert_ulps_eq!(edge_rank.0[2], 0.05);
+
+            let (ranks, superstep) = dbg!(line_rank(&graph, 10, 0.85)).unwrap();
+            assert_eq!(ranks.len(), 4);
+            assert_ulps_eq!(ranks[0], 0.128625);
+            assert_ulps_eq!(ranks[1], 0.221125);
+            assert_ulps_eq!(ranks[2], 0.1425);
+            assert_ulps_eq!(ranks[3], 0.05);
+            assert_eq!(superstep, 3);
+        }
+
+        #[test]
+        fn circle() {
+            let graph = Graph::create(
+                vec!["V/A".to_string(), "V/B".to_string(), "V/C".to_string()],
+                vec![
+                    ("V/A".to_string(), "V/B".to_string()),
+                    ("V/B".to_string(), "V/C".to_string()),
+                    ("V/C".to_string(), "V/A".to_string()),
+                ],
+            );
+            let mut edge_rank = EdgeVector(vec![1. / 3.; graph.number_of_edges() as usize]);
+            let normalization = edge_normalization(&graph);
+
+            next_superstep(&mut edge_rank, &graph, &normalization, 0.85);
+            assert_ulps_eq!(edge_rank.0[0], 1. / 3.);
+            assert_ulps_eq!(edge_rank.0[1], 1. / 3.);
+            assert_ulps_eq!(edge_rank.0[2], 1. / 3.);
+
+            let (ranks, superstep) = dbg!(line_rank(&graph, 10, 0.85).unwrap());
+
+            assert_eq!(ranks.len(), 3);
+            assert_ulps_eq!(ranks[0], 2. / 3.);
+            assert_ulps_eq!(ranks[1], 2. / 3.);
+            assert_ulps_eq!(ranks[2], 2. / 3.);
+            assert_eq!(superstep, 0);
+        }
+    }
 }
--- src/algorithms/linerank/graph_multiplications.rs
+++ src/algorithms/linerank/graph_multiplications.rs
@@ -1,11 +1,10 @@
 use crate::graph_store::graph::Graph;
 
+#[derive(Clone, Debug)]
 pub struct EdgeVector(pub Vec<f64>);
 impl EdgeVector {
-    // source matrix transposed times self
-    pub fn project_to_edge_sources(self, graph: &Graph) -> VertexVector {
-        // TODO make sure that graph has no dangling edges
-        // TODO make sure that vec has as many entries as edges in graph
+    pub fn apply_transposed_source_matrix(self, graph: &Graph) -> VertexVector {
+        assert_eq!(self.0.len(), graph.number_of_edges() as usize);
         let mut result: Vec<f64> = vec![0.; graph.number_of_vertices() as usize];
         graph
             .edges
@@ -15,13 +14,43 @@ impl EdgeVector {
             .for_each(|(edge_id, source)| result[source] += self.0[edge_id]);
         VertexVector(result)
     }
+    pub fn apply_transposed_target_matrix(self, graph: &Graph) -> VertexVector {
+        assert_eq!(self.0.len(), graph.number_of_edges() as usize);
+        let mut result: Vec<f64> = vec![0.; graph.number_of_vertices() as usize];
+        graph
+            .edges
+            .iter()
+            .map(|e| e.to().to_usize())
+            .enumerate()
+            .for_each(|(edge_id, source)| result[source] += self.0[edge_id]);
+        VertexVector(result)
+    }
+    pub fn invert_elementwise(self) -> Self {
+        Self(
+            self.0
+                .into_iter()
+                .map(|x| match x {
+                    x if approx::ulps_eq!(x, 0.) => 0.,
+                    x => 1. / x,
+                })
+                .collect(),
+        )
+    }
+    pub fn normalize_with(self, normalization: &Self) -> Self {
+        Self(
+            self.0
+                .into_iter()
+                .zip(normalization.0.iter())
+                .map(|(x, norm)| x * norm)
+                .collect(),
+        )
+    }
 }
+#[derive(Debug)]
 pub struct VertexVector(pub Vec<f64>);
 impl VertexVector {
-    // target matrix times self
-    pub fn project_to_incoming_edges(self, graph: &Graph) -> EdgeVector {
-        // TODO make sure that graph has no dangling edges
-        // TODO make sure that vec has as many entries as vertices in graph
+    pub fn apply_target_matrix(self, graph: &Graph) -> EdgeVector {
+        assert_eq!(self.0.len(), graph.number_of_vertices() as usize);
         EdgeVector(
             graph
                 .edges
@@ -30,6 +59,12 @@ impl VertexVector {
                 .map(|target| self.0[target])
                 .collect(),
         )
+    }
+}
+impl std::ops::Add for VertexVector {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0.into_iter().zip(rhs.0).map(|(x, y)| x + y).collect())
     }
 }
 
@@ -50,7 +85,7 @@ mod tests {
             ],
         );
 
-        let result = VertexVector(vec![1., 0.1, 1.5]).project_to_incoming_edges(&graph);
+        let result = VertexVector(vec![1., 0.1, 1.5]).apply_target_matrix(&graph);
 
         assert_eq!(result.0.len() as u64, graph.number_of_edges());
         assert_ulps_eq!(result.0[0], 1.);
@@ -71,7 +106,7 @@ mod tests {
             ],
         );
 
-        let result = EdgeVector(vec![1., 0.1, 1.5, 10.3]).project_to_edge_sources(&graph);
+        let result = EdgeVector(vec![1., 0.1, 1.5, 10.3]).apply_transposed_source_matrix(&graph);
 
         assert_eq!(result.0.len() as u64, graph.number_of_vertices());
         assert_ulps_eq!(result.0[0], 1. + 0.1 + 1.5);
